8e74d9e256f41908fe0d36da1f85dbc6684263e3
Drake Stephens <meuio56@gmail.com>
2020-08-15 03:06:34 -0600

    I'm done

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const session = require('express-session');
+const SequalizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helper');
+
+const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
-const session = require('express-session');
-const SequalizeStore = require('connect-session-sequelize')(session.Store)
+
+
 const sess = {
   secret: 'Super secret secret',
   cookie:{},
